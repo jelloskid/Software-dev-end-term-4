2fca589b7235f76105095af0eb481c6bb97e75b5
sean <seanvn2005@gmail.com>
2022-11-14 11:25:53 +0200

    css border styling
    styled css

---
+++ js/validate.js
@@ -0,0 +1,13 @@
+function newtask(){
+
+//dom ipi refrences
+//1.get the new task element/input by its own id
+//2.create an if statement to prevent html characters
+if (true) {
+    
+document.getElementById("error").style.display ="block"
+}
+
+}
+
+newtask()
