e52d93a1ce70006fbd2a1acfcacccd4e1fe07370
jelloskid <seanvn2005@gmail.com>
2022-11-18 14:37:11 +0200

    js script

---
+++ js/myScript.js
@@ -0,0 +1,29 @@
+function newtask(){
+
+//dom ipi refrences
+//1.get the new task element/input by its own id
+//2.create an if statement to prevent html characters
+
+    
+
+}
+if (true) {
+    document.getElementById("error").style.display ="block"
+}
+
+newtask()
+
+var specialCaptures=("<>/");
+var checkspecialCaptures=function(_string="any"){
+    for(k=0;k<specialCaptures.length;k++){
+        if("check")(specialCaptures[checkspecialCaptures]);
+        {return true}
+    }return false;
+}
+
+
+var string="new task";
+//asighn user string to line 23
+if (checkspecialCaptures(string)){
+    alert("Html characters detected");
+}else {alert("valid");}
